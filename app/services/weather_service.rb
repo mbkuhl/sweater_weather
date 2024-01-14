2d9f36192b9b474092598aa90f18dac6338d8c3e
Michael Kuhlmeier <kuhlmmb@gmail.com>
2024-01-14 00:32:07 -0700

    Clean up weather array that gets passed to facade

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -11,6 +11,41 @@ class WeatherService
   end
 
   def self.forecast_cleaner(full_hash)
-    full_hash
+    current = full_hash[:current]
+    days = full_hash[:forecast][:forecastday]
+    hours = full_hash[:forecast][:forecastday].first[:hour]
+    clean_hash = {
+      current_weather: {
+        last_updated: current[:last_updated],
+        temperature: current[:temp_f],
+        feels_like: current[:feelslike_f],
+        humidity: current[:humidity],
+        uvi: current[:uv],
+        visibility: current[:vis_miles],
+        condition: current[:condition][:text],
+        icon: current[:condition][:icon]
+      },
+      daily_weather: days.map do |day|
+        d = day[:day]
+        astro = day[:astro]
+        {
+          date: day[:date],
+          sunrise: astro[:sunrise],
+          sunset: astro[:sunset],
+          max_temp: d[:maxtemp_f],
+          min_temp: d[:mintemp_f],
+          condition: d[:condition][:text],
+          icon: d[:condition][:icon]
+        }
+      end,
+      hourly_weather: hours.map do |hour|
+        {
+          time: hour[:time].split(" ")[2],
+          temperature: hour[:temp_f],
+          conditions: hour[:condition][:text],
+          icon: hour[:condition][:icon]
+        }
+      end
+    }
   end
 end
