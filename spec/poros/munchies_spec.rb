b51d7558f43bc10b2ec1d48149fa4901f4660980
Michael Kuhlmeier <kuhlmmb@gmail.com>
2024-01-14 14:12:24 -0700

    Add testing for munchies poro

---
+++ spec/poros/munchies_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Munchies do
+  it 'object has all attributes if given proper data', :vcr do
+    coordinates_hash = MapquestService.get_lat_lon("houston,tx")
+    restaurant_data = YelpService.get_restaurant("italian", coordinates_hash[:lat], coordinates_hash[:lng])
+    forecast_data = WeatherService.get_forecast_for_munchies(coordinates_hash[:lat], coordinates_hash[:lng])
+    munchies = Munchies.new(restaurant_data, forecast_data, "houston,tx")
+
+    expect(munchies).to be_a Munchies
+
+    expect(munchies.location).to eq("Houston, TX")
+    expect(munchies.forecast).to be_a Hash
+    expect(munchies.forecast.keys).to eq([:temperature, :condition])
+    expect(munchies.forecast[:temperature]).to be_a Float
+    expect(munchies.forecast[:condition]).to be_a String
+    expect(munchies.restaurant).to be_a Hash
+    expect(munchies.restaurant.keys).to eq([:name, :address, :rating, :reviews])
+    expect(munchies.restaurant[:name]).to be_a String
+    expect(munchies.restaurant[:address]).to be_a String
+    expect(munchies.restaurant[:rating]).to be_a Float
+    expect(munchies.restaurant[:reviews]).to be_a Integer
+
+  end
+end
