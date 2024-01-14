de1a62502eab90e7eebd6359dd2c440938d33b6a
Michael Kuhlmeier <kuhlmmb@gmail.com>
2024-01-14 14:16:12 -0700

    Add first portion of munchies request test

---
+++ spec/requests/api/v0/munchies/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Munchies Show" do
+  it "Get request for munchies show", :vcr do
+    get "/api/v1/munchies?destination=pueblo,co&food=italian"
+
+    expect(response).to be_successful
+    
+    json_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json_response).to be_a Hash
+    expect(json_response[:data]).to be_a Hash
+    expect(json_response[:data][:id]).to be nil
+    expect(json_response[:data][:type]).to eq("munchies")
+    data = json_response[:data][:attributes]
+    expect(data).to be_a Hash
+
+  end
+end
